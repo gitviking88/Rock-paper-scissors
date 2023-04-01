e7425fbdbc862ebcbb4591c2d536d6455586571b
gitviking88 <nnorup@hotmail.com>
2023-04-01 20:05:20 +0200

    re-write to switch loop

--- rpsgame.js
+++ rpsgame.js
@@ -8,32 +8,60 @@ function playRound(playerSelection, computerSelection) {
     
     if (computerSelection === playerSelection) {
      return "it's a tie"; 
-    } else if (
-        (computerSelection === "paper" && playerSelection === "rock") || 
-        (computerSelection === "rock" && playerSelection === "scissors") || 
-        (computerSelection === "scissors" && playerSelection === "paper") 
-        ) 
-    {
-        return "You Lose," + " " + computerSelection + " beats " + playerSelection + ".";
-    } else {
-        return "You Win," + " " + playerSelection + " beats " + computerSelection +".";
     }
+
+    switch (true){
+        case (computerSelection === "paper" && playerSelection === "rock") :
+        case (computerSelection === "rock" && playerSelection === "scissors") :
+        case (computerSelection === "scissors" && playerSelection === "paper") :
+            return "You Lose," + " " + computerSelection + " beats " + playerSelection + ".";
+            break;
+        default :
+            return "You Win," + " " + playerSelection + " beats " + computerSelection +".";
+    }
+
+
+
+    // if (
+    //     (computerSelection === "paper" && playerSelection === "rock") || 
+    //     (computerSelection === "rock" && playerSelection === "scissors") || 
+    //     (computerSelection === "scissors" && playerSelection === "paper")
+    //     ) 
+    // {
+    //     return "You Lose," + " " + computerSelection + " beats " + playerSelection + ".";
+    // } 
+    // if (
+    //     (playerSelection != "rock") ||
+    //     (playerSelection != "paper") ||
+    //     (playerSelection != "scissors")
+    //     )
+    // {
+    //     console.log("Player select: " + playerSelection)
+    //     return "Error, please type either 'rock', 'paper' or 'scissors'";
+    // }
+    // return "You Win," + " " + playerSelection + " beats " + computerSelection +".";
+    
 }
 
 function game() {
     let scorePlayer = 0;
     let scoreComputer = 0;   
         for (let i = 0; i < 5; i++) {
-            const playerSelection = prompt("Choose Rock, Paper or Scissors").toLowerCase();
+            let playerSelection = prompt("Choose Rock, Paper or Scissors").toLowerCase();
+            while (!["rock", "paper", "scissors"].includes(playerSelection)) {
+                playerSelection = prompt("Invalid input. Please choose Rock, Paper or Scissors").toLowerCase();
+            }
             const computerSelection = getComputerChoice().toLowerCase();
-            console.log(playerSelection);
-            console.log(computerSelection);
+            console.log("Player: " + playerSelection);
+            console.log("Computer: " + computerSelection);
             let winner = playRound(playerSelection, computerSelection);
             console.log(winner);
             if (winner.charAt(4) === "W") {
             scorePlayer++;
             } else if (winner.charAt(4) === "L") {
             scoreComputer++;
+            } else if (winner.startsWith("Error")) {
+                i + 0;
             }
         }
     if (scorePlayer === scoreComputer) {
